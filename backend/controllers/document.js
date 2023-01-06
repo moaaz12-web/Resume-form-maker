58b5e81bcf95f4a3cd9f1a46c52e0c395ec455cd
Shayan Jamil <shayanjamil500@gmail.com>
2023-01-06 23:02:51 +0500

    Backend Completed

--- backend/controllers/document.js
+++ backend/controllers/document.js
@@ -46,7 +46,7 @@ module.exports = {
             const updatedDocument = await Document.findById(req.params.id);
             updatedDocument.isFavourite = true;
             await updatedDocument.save();
-            res.status(200).json({ message: "document updated successfully" });
+            res.status(200).json({ message: "document favourited successfully" });
         } catch (error) {
             res.json(error);
         }
